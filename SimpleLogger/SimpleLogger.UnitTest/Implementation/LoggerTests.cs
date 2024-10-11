ffe1314f3c985602ab021609456f3285534152c7
Barnabas_Szegleti <szegletibarnabas@gmail.com>
2024-10-11 13:10:02 +0200

    Basic UnitTests

---
+++ SimpleLogger/SimpleLogger.UnitTest/Implementation/LoggerTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SimpleLogger.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLogger.UnitTest.Implementation
+{
+    [TestFixture]
+    public class LoggerTests
+    {
+        Logger _testLogger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _testLogger = new Logger();
+        }
+
+        [Test]
+        public void LogToEventList_AddingNewItem()
+        {
+
+            _testLogger.LogToEventList("AppleTree");
+
+            _testLogger.LoggedEvents.Count().Should().Be(1);
+            _testLogger.LoggedEvents.Should().Contain("AppleTree");
+        }
+
+        [Test]
+        public void LogToFile()
+        {
+            var fileName = "\\SimpleLog" + DateTime.Today.Date.ToString() + ".txt";
+            var directoryPath = Path.Combine(RuntimeEnvironment.GetRuntimeDirectory(), "\\SimpleLoggerLogs");
+            string path = Path.Combine(directoryPath, fileName);
+
+            _testLogger.LogToFile("AppleTree");
+
+            Assert.That(System.IO.Directory.Exists(directoryPath));
+            Assert.That(System.IO.File.Exists(path));
+        }
+
+        [Test]
+        public void SetupLogPath_ReturnsUserInputPath()
+        {
+            var path = "AppleTree";
+
+            _testLogger.LogPath = path;
+
+            _testLogger.LogPath.Should().Be(path);
+        }
+
+
+    }
+}
---
+++ SimpleLogger/SimpleLogger.UnitTest/SimpleLoggerManagerTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SimpleLogger.DomainModel;
+using SimpleLogger.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLogger.UnitTest
+{
+    [TestFixture]
+    public class SimpleLoggerManagerTests
+    {
+        private SimpleLoggerManager _testManager;
+        private Mock<ILogger> _loggerMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger>();
+            List<string> events = new List<string>() { "Hello World","Nice Try"};
+            _loggerMock.Setup(n=>n.LoggedEvents).Returns(events);
+
+            _testManager = new SimpleLoggerManager(_loggerMock.Object);
+        }
+
+        [TestCase("AppleTree")]
+        public void SetUpLogPath(string logpath)
+        {
+            _testManager.SetUpLogPath(logpath);
+
+            _loggerMock.Verify(n => n.LogPath, Times.Once());
+        }
+
+
+        [TestCase(LogTarget.File,"Alma")]
+        public void LogToFile_CallsCorrectMethod(LogTarget logTarget, string message)
+        {
+            _testManager.Log(logTarget, message);
+
+            _loggerMock.Verify(n => n.LogToEventList(message), Times.Never());
+            _loggerMock.Verify(n => n.LogToFile(message), Times.Once());
+        }
+
+        [TestCase(LogTarget.EventLog, "Alma")]
+        public void LogToEventList_CallsCorrectMethod(LogTarget logTarget, string message)
+        {
+            _testManager.Log(logTarget, message);
+
+            _loggerMock.Verify(n => n.LogToEventList(message), Times.Once());
+            _loggerMock.Verify(n => n.LogToFile(message), Times.Never());
+        }
+
+        [Test]
+        public void EventLog_ReturnsValidCollection()
+        {
+            List<string> events = new List<string>() { "Hello World", "Nice Try" };
+            _loggerMock.Setup(n => n.LoggedEvents).Returns(events);
+
+            var result = _testManager.GetEventLog();
+
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(events);
+        }
+
+        [Test]
+        public void EventLog_ReturnsEmptyCollection()
+        {
+
+            var result = _testManager.GetEventLog();
+
+            result.Should().NotBeNull();
+            result.Count.Should().Be(0);
+        }
+
+    }
+}
